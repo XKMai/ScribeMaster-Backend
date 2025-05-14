f5ab198cf334e2e48be3462341b792876e7715c8
XKMai <163821858+XKMai@users.noreply.github.com>
2025-05-14 14:59:34 +0800

    Initial setup of server with Fastify

---
+++ main.ts
@@ -0,0 +1,27 @@
+import Fastify from 'fastify';
+import userRoutes from './routes/users';
+
+const fastify = require('fastify')({logger:true})
+fastify.register(require('@fastify/jwt'), {
+  secret: 'supersecretstring'  //Secret string
+})
+
+const PORT = 5000;
+
+
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+})
+
+
+const start = async () => {
+    try {
+        await fastify.listen({port : PORT})
+    } catch (error) {
+    fastify.log.error(error)
+    process.exit(1)
+    }   
+}
+
+start()
