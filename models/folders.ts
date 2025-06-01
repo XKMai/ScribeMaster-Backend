84a73f46d842e74591df857be57a827ee2b40c3d
XKMai <xkmai014@gmail.com>
2025-06-01 22:39:27 +0800

    Created folder schema

---
+++ models/folders.ts
@@ -0,0 +1,10 @@
+import { integer, pgTable, varchar, boolean, json} from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const folders = pgTable("folders", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  isCampaign: boolean().default(false),
+  settings: json(), // campaign config
+  createdBy: integer().references(() => users.id).notNull()
+});
